702eb8f73ff90a532e0bf09f0c7d7860c0b9e683
Michael Himing <mhiming@gmail.com>
2016-09-30 10:37:42 +1000

    Init

---
+++ emojiman.py
@@ -0,0 +1,34 @@
+import random
+
+import emoji
+import tweepy
+
+import settings
+
+
+def main():
+    auth = tweepy.OAuthHandler(*settings.oauth)
+    auth.set_access_token(*settings.access_token)
+    api = tweepy.API(auth)
+
+    me = api.me()
+
+    name = me.name
+
+    emojis = [v for k, v in emoji.EMOJI_UNICODE.iteritems() if not k.startswith(':flag_for_')]
+
+    print 'Current name:', name
+
+    for e in emojis:
+        if name.endswith(e):
+            name = name[:-len(e)]
+
+    name += random.choice(emojis)
+
+    print 'New name:', name
+
+    api.update_profile(name=name)
+
+
+if __name__ == '__main__':
+    main()
